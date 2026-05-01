71eea37225f72e61a52d0c7571222fec93eab9b8
Ygal Blum <ygal.blum@gmail.com>
2026-05-01 09:02:49 -0400

    PetClinic Item Seed - use nginx from redhat registry (#81)
    Backport #79
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- api/v1alpha1/servicetypes/three_tier_app_demo/constants.go
+++ api/v1alpha1/servicetypes/three_tier_app_demo/constants.go
@@ -5,4 +5,8 @@ package three_tier_app_demo
 const (
 	DefaultDatabaseEngine  = "postgres"
 	DefaultDatabaseVersion = "18"
+
+	// constants for the three_tier_app_demo Pet Clinic CatalogItem
+	WebImage = "registry.access.redhat.com/ubi9/nginx-126:9.7"
+	AppImage = "docker.io/springcommunity/spring-framework-petclinic:6.1.2"
 )
